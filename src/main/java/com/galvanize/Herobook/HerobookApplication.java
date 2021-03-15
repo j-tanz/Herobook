cb807f28c5cfa017f7b46b6decfd4336483f456b
j-tanz <john.tansor@cognizant.com>
2021-03-15 18:52:48 -0400

    init commit

---
+++ src/main/java/com/galvanize/Herobook/HerobookApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.Herobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerobookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerobookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/Herobook/HerobookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.Herobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
