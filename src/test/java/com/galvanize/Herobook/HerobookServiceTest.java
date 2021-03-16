642a944f26f3414a86fb3fadfbfe4a5c2edc67e7
cld71 <cld71@cld-pc2.diehl>
2021-03-16 17:45:52 -0500

    Acceptance 1 - Green Test - Testing service.

--- src/test/java/com/galvanize/Herobook/HerobookServiceTest.java
+++ src/test/java/com/galvanize/Herobook/HerobookServiceTest.java
@@ -1,9 +1,15 @@
 package com.galvanize.Herobook;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class HerobookServiceTest {
     @Mock
     HerobookRepository herobookRepository;
@@ -13,7 +19,10 @@ public class HerobookServiceTest {
 
     @Test
     public void CreateHeroBook(){
-        HeroBook heros=new HeroBook();
-
+        HeroBook heros=new HeroBook(null,"Bruce Wayne","Batman","6'5\"","200","he does not possess any superpowers","high","high","90","25","good","I am batman","I am batman");
+        this.herobookService.createHerobook(heros);
+        verify(this.herobookRepository).save(
+                new HeroBook(null,"Bruce Wayne","Batman","6'5\"","200","he does not possess any superpowers","high","high","90","25","good","I am batman","I am batman")
+        );
     }
 }
